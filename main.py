9763d1455dc9bee0fcdba35ccaeaf8a221314e5e
Sam <sams421@icloud.com>
2025-01-08 20:31:15 +0000

    Separated into class-based system, introduced repeat solving option

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from detect import screenshot_board, process_board_into_memory, output
-from solve import process
+from solve import Solver
 from imports import print_grid
 from time import sleep
 
@@ -8,18 +8,22 @@ def run():
     total_changes = 1
     unseen = 1
 
-    old_grid = None
+    solver = Solver()
 
     while total_changes > 0 and unseen > 0:
 
         board, left, top, right, bottom = screenshot_board()
         grid = process_board_into_memory(board)
-        grid = process(grid, old_grid=old_grid)
-        print_grid(grid)
-        total_changes, unseen = output(grid, old_grid, left, top, right, bottom)
 
-        old_grid = grid
-        sleep(max(0.75, total_changes * 0.015 + 0.3))
+        old_grid = solver.copy(grid)
+
+        solver.solve(grid, repeat_solving=False, use_old_grid=True, total_mines=99)
+
+        print_grid(solver.grid)
+
+        total_changes, unseen = output(solver.grid, old_grid, left, top, right, bottom)
+
+        sleep(max(0.75, total_changes * 0.01 + 0.4))
 
 if __name__ == '__main__':
 
--- solve.py
+++ solve.py
@@ -5,241 +5,285 @@ from imports import UNSEEN_CONST, MINE_CONST, CHANGE_CONST, EMPTY_CONST, print_g
 import warnings
 
 
-def process(grid, old_grid=None, total_mines = 99):
+class Solver:
 
-    width = len(grid[0])
-    height = len(grid)
+    def __init__(self):
+        self.old_grid = None
 
-    # Check compatability of old grid
+    def solve(self, grid, repeat_solving=True, use_old_grid=True, total_mines=99):
 
-    if old_grid is not None:
-        if width != len(old_grid[0]) or height != len(old_grid):
-            raise Exception("Grids are different sizes")
-                    
+        self.grid = grid
+        self.width = len(grid[0])
+        self.height = len(grid)
+        self.total_mines = total_mines
 
-    # Find all numbers
+        # Check compatability of old grid
 
-    number_cells = []
+        if self.old_grid is not None:
+            if self.width != len(self.old_grid[0]) or self.height != len(self.old_grid):
+                raise Exception("Grids are different sizes")
+                        
 
-    for row in range(height):
-        for col in range(width):
-            if grid[row][col] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
-                number_cells.append((row, col))
+        # Find all numbers
 
-                # Find discrepancies with old grid and output warnings
-                if old_grid is not None:
-                    if grid[row][col] != old_grid[row][col] and old_grid[row][col] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
-                        warnings.warn(f"Error: grids differ at ({col}, {row}), holding old value {old_grid[row][col]} and new value {grid[row][col]}")
-                        grid[row][col] = old_grid[row][col]
+        self.number_cells = []
 
+        for row in range(self.height):
+            for col in range(self.width):
+                if self.grid[row][col] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
+                    self.number_cells.append((row, col))
 
-            # Setting mines that were lost since the previous round
-            if old_grid is not None:
-
-                if old_grid[row][col] == MINE_CONST:
-
-                    grid[row][col] = MINE_CONST
+                    # Find discrepancies with old grid and output warnings
+                    if self.old_grid is not None:
+                        if self.grid[row][col] != self.old_grid[row][col] and self.old_grid[row][col] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
+                            warnings.warn(f"Error: grids differ at ({col}, {row}), holding old value {self.old_grid[row][col]} and new value {self.grid[row][col]}")
+                            self.grid[row][col] = self.old_grid[row][col]
 
 
-    detect_mine_q = deque(number_cells)
+                # Setting mines that were lost since the previous round
+                if self.old_grid is not None:
 
-    # If q is empty, set a click in the middle
+                    if self.old_grid[row][col] == MINE_CONST:
 
-    if len(detect_mine_q) == 0:
+                        self.grid[row][col] = MINE_CONST
 
-        grid[height // 2][width // 2] = CHANGE_CONST
+        # If q is empty, set a click in the middle
+
+        if len(self.number_cells) == 0:
+
+            self.grid[self.height // 2][self.width // 2] = CHANGE_CONST
+            return grid
+
+        self.detect_mines()
+
+        self.round_changes = 1
+
+        # Runs multiple rounds of attempting to solve, stopping when no further information frm a round can be given
+
+        while self.round_changes > 0:
+
+            self.round_changes = 0
+
+            self.clear_cells()
+            self.check_mines_valid()
+            self.trial_mines()
+
+            if not repeat_solving:
+                break
+
+        self.check_complete()
+
+        if use_old_grid:
+            self.old_grid = self.copy(grid)
+
         return grid
 
-    # Check all numbers - if there are n unseen non-mines, we declare those as mines
+    def copy(self, grid):
+        return [[cell for cell in row] for row in grid]
 
-    while detect_mine_q:
+    def reset(self):
+        self.old_grid = None
 
-        y, x = detect_mine_q.popleft()
+    def detect_mines(self):
 
-        num = int(grid[y][x])
-        
-        poss_mines = 0
-        use_unseen = False
+        # Check all numbers - if there are n unseen non-mines, we declare those as mines
 
-        unseen = []
-        mines = []
-        other = []
+        detect_mine_q = deque(self.number_cells)
 
-        for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-            if 0 <= y + dy < height and 0 <= x + dx < width:
-                if grid[y + dy][x + dx] in (UNSEEN_CONST, MINE_CONST):
-                    poss_mines += 1
+        while detect_mine_q:
 
-                    if grid[y + dy][x + dx] == UNSEEN_CONST:
-                        use_unseen = True
-                        unseen.append((y + dy, x + dx))
-                    else:
+            y, x = detect_mine_q.popleft()
+
+            num = int(self.grid[y][x])
+            
+            poss_mines = 0
+            use_unseen = False
+
+            unseen = []
+            mines = []
+            other = []
+
+            for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+                if 0 <= y + dy < self.height and 0 <= x + dx < self.width:
+                    if self.grid[y + dy][x + dx] in (UNSEEN_CONST, MINE_CONST):
+                        poss_mines += 1
+
+                        if self.grid[y + dy][x + dx] == UNSEEN_CONST:
+                            use_unseen = True
+                            unseen.append((y + dy, x + dx))
+                        else:
+                            mines.append((y + dy, x + dx))
+                    elif self.grid[y + dy][x + dx] != EMPTY_CONST:
+                        other.append((y + dy, x + dx))
+
+            if num == poss_mines and use_unseen:
+                for y, x in unseen:
+                    self.grid[y][x] = MINE_CONST
+
+                for y, x in other:
+                    detect_mine_q.append((y, x))
+
+    def clear_cells(self):
+
+        # Clearing cells where mine requirement is satisfied
+
+        for y, x in self.number_cells:
+
+            mines = []
+            unseen = []
+
+            num = int(self.grid[y][x])
+
+            for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+
+                if 0 <= y + dy < self.height and 0 <= x + dx < self.width:
+                    if self.grid[y + dy][x + dx] == MINE_CONST:
                         mines.append((y + dy, x + dx))
-                elif grid[y + dy][x + dx] != EMPTY_CONST:
-                    other.append((y + dy, x + dx))
-
-        if num == poss_mines and use_unseen:
-            for y, x in unseen:
-                grid[y][x] = MINE_CONST
-
-            for y, x in other:
-                detect_mine_q.append((y, x))
-
-    # Clearing cells where mine requirement is satisfied
-    for y, x in number_cells:
-
-        mines = []
-        unseen = []
-
-        num = int(grid[y][x])
-
-        for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-
-            if 0 <= y + dy < height and 0 <= x + dx < width:
-                if grid[y + dy][x + dx] == MINE_CONST:
-                    mines.append((y + dy, x + dx))
-                elif grid[y + dy][x + dx] == UNSEEN_CONST:
-                    unseen.append((y + dy, x + dx))
+                    elif self.grid[y + dy][x + dx] == UNSEEN_CONST:
+                        unseen.append((y + dy, x + dx))
+            
+            if len(mines) == num:
+                for y, x in unseen:
+                    self.grid[y][x] = CHANGE_CONST
+                    self.round_changes += 1
         
-        if len(mines) == num:
-            for y, x in unseen:
-                grid[y][x] = CHANGE_CONST
+    def check_mines_valid(self):
 
-    # Checking all mines are valid
+        # Checking all mines are valid
 
-    for y, x in number_cells:
+        for y, x in self.number_cells:
 
-        # Count surrounding mines
+            # Count surrounding mines
 
-        num_mines = 0
-
-        for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-
-            if 0 <= y + dy < height and 0 <= x + dx < width:
-                if grid[y + dy][x + dx] == MINE_CONST:
-
-                    num_mines += 1
-        
-        if num_mines > int(grid[y][x]):
-            print_grid(grid)
-            raise Exception(f"The number of mines is invalid for this board. Check position ({y+1}, {x+1}), with anticipated value {grid[y][x]}. This error is likely due to the computer vision process reading the board incorrectly.")
-        
-
-    # Trialling mines on each number
-
-    for y, x in number_cells:
-
-        # Gathering unseen
-        num = int(grid[y][x])
-        num_mines = 0
-        unseen = []
-
-        for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-
-            if 0 <= y + dy < height and 0 <= x + dx < width:
-                if grid[y + dy][x + dx] == UNSEEN_CONST:
-                    unseen.append((y + dy, x + dx))
-                elif grid[y + dy][x + dx] == MINE_CONST:
-                    num_mines += 1
-
-        if len(unseen) == 0:
-            continue
-
-        # Collecting numbers around unseen
-        sensitive_cells = []    
-
-        for a, b in unseen:
-            for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-
-                if 0 <= a + dy < height and 0 <= b + dx < width:
-                    if grid[a + dy][b + dx] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
-                        sensitive_cells.append((a + dy, b + dx))
-    
-        def test_sensitive(y1, x1):
-            value = int(grid[y1][x1])
             num_mines = 0
-            num_unseen = 0
+
             for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
-                if 0 <= y1 + dy < height and 0 <= x1 + dx < width:
-                    if grid[y1 + dy][x1 + dx] == MINE_CONST:
+
+                if 0 <= y + dy < self.height and 0 <= x + dx < self.width:
+                    if self.grid[y + dy][x + dx] == MINE_CONST:
+
+                        num_mines += 1
+            
+            if num_mines > int(self.grid[y][x]):
+                print_grid(self.grid)
+                raise Exception(f"The number of mines is invalid for this board. Check position ({y+1}, {x+1}), with anticipated value {self.grid[y][x]}. This error is likely due to the computer vision process reading the board incorrectly.")
+            
+    def trial_mines(self):
+        # Trialling mines on each number
+
+        for y, x in self.number_cells:
+
+            # Gathering unseen
+            num = int(self.grid[y][x])
+            num_mines = 0
+            unseen = []
+
+            for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+
+                if 0 <= y + dy < self.height and 0 <= x + dx < self.width:
+                    if self.grid[y + dy][x + dx] == UNSEEN_CONST:
+                        unseen.append((y + dy, x + dx))
+                    elif self.grid[y + dy][x + dx] == MINE_CONST:
                         num_mines += 1
 
-                    elif grid[y1 + dy][x1 + dx] == UNSEEN_CONST:
-                        num_unseen += 1
+            if len(unseen) == 0:
+                continue
 
-            return num_mines <= value <= num_mines + num_unseen
+            # Collecting numbers around unseen
+            sensitive_cells = []    
 
+            for a, b in unseen:
+                for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
 
-        # Creating array to examine validities
-        valid = [[False, False] for _ in range(len(unseen))]
-
-        # Modelling the unseen mines
-        for i in range(2**len(unseen)):
-            bin_arr = list(map(int, bin(i)[2:].zfill(len(unseen))))
-
-            for j in range(len(unseen)):
-
-                unseen_y, unseen_x = unseen[j]
-
-                if bin_arr[j]:
-                
-                    grid[unseen_y][unseen_x] = MINE_CONST
-
-                else:
-                    grid[unseen_y][unseen_x] = CHANGE_CONST
-
-            valid_model = True
-
-            for a, b in sensitive_cells:
-                if not test_sensitive(a, b):
-                    valid_model = False
-                    break
-
-            if valid_model:
-
-                for j, value in enumerate(bin_arr):
-                    valid[j][int(value)] = True
-
-        # Resetting all of unseen
-        for y, x in unseen:
-            grid[y][x] = UNSEEN_CONST
-
-        # If only one choice is valid, we set for this specific cell
-
-        for i in range(len(valid)):
-            if valid[i][0] ^ valid[i][1]:
-
-                unseen_y, unseen_x = unseen[i]
-
-                if valid[i][0]:
-
-                    grid[unseen_y][unseen_x] = CHANGE_CONST
-
-                else:
-                    grid[unseen_y][unseen_x] = MINE_CONST
-
-    # Computing number of mines and number of unseen
-
-    unseen = []
-    num_mines = 0
-    for y in range(height):
-        for x in range(width):
-
-            if grid[y][x] == UNSEEN_CONST:
-                unseen.append((y, x))
-
-            elif grid[y][x] == MINE_CONST:
-                num_mines += 1
-
-
-    if num_mines == total_mines:
-        for a, b in unseen:
-            grid[a][b] = CHANGE_CONST
-    
-    elif num_mines + len(unseen) == total_mines:
-        for a, b in unseen:
-            grid[a][b] = MINE_CONST
-
+                    if 0 <= a + dy < self.height and 0 <= b + dx < self.width:
+                        if self.grid[a + dy][b + dx] not in (UNSEEN_CONST, MINE_CONST, EMPTY_CONST, CHANGE_CONST):
+                            sensitive_cells.append((a + dy, b + dx))
         
+            def test_sensitive(y1, x1):
+                value = int(self.grid[y1][x1])
+                num_mines = 0
+                num_unseen = 0
+                for dy, dx in ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)):
+                    if 0 <= y1 + dy < self.height and 0 <= x1 + dx < self.width:
+                        if self.grid[y1 + dy][x1 + dx] == MINE_CONST:
+                            num_mines += 1
 
-    return grid
+                        elif self.grid[y1 + dy][x1 + dx] == UNSEEN_CONST:
+                            num_unseen += 1
+
+                return num_mines <= value <= num_mines + num_unseen
+
+
+            # Creating array to examine validities
+            valid = [[False, False] for _ in range(len(unseen))]
+
+            # Modelling the unseen mines
+            for i in range(2**len(unseen)):
+                bin_arr = list(map(int, bin(i)[2:].zfill(len(unseen))))
+
+                for j in range(len(unseen)):
+
+                    unseen_y, unseen_x = unseen[j]
+
+                    if bin_arr[j]:
+                    
+                        self.grid[unseen_y][unseen_x] = MINE_CONST
+
+                    else:
+                        self.grid[unseen_y][unseen_x] = CHANGE_CONST
+
+                valid_model = True
+
+                for a, b in sensitive_cells:
+                    if not test_sensitive(a, b):
+                        valid_model = False
+                        break
+
+                if valid_model:
+
+                    for j, value in enumerate(bin_arr):
+                        valid[j][int(value)] = True
+
+            # Resetting all of unseen
+            for y, x in unseen:
+                self.grid[y][x] = UNSEEN_CONST
+
+            # If only one choice is valid, we set for this specific cell
+
+            for i in range(len(valid)):
+                if valid[i][0] ^ valid[i][1]:
+
+                    unseen_y, unseen_x = unseen[i]
+
+                    if valid[i][0]:
+
+                        self.grid[unseen_y][unseen_x] = CHANGE_CONST
+                        self.round_changes += 1
+
+                    else:
+                        self.grid[unseen_y][unseen_x] = MINE_CONST
+                        self.round_changes += 1
+
+    def check_complete(self):
+
+        # Computing number of mines and number of unseen
+
+        unseen = []
+        num_mines = 0
+        for y in range(self.height):
+            for x in range(self.width):
+
+                if self.grid[y][x] == UNSEEN_CONST:
+                    unseen.append((y, x))
+
+                elif self.grid[y][x] == MINE_CONST:
+                    num_mines += 1
+
+        if num_mines == self.total_mines:
+            for a, b in unseen:
+                self.grid[a][b] = CHANGE_CONST
+        
+        elif num_mines + len(unseen) == self.total_mines:
+            for a, b in unseen:
+                self.grid[a][b] = MINE_CONST
+
